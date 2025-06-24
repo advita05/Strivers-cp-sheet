2c036aa4f31f4cf3efbb6a2ee1cb0e4706e6e6dc
Advita Talekar <advita.18060@sakec.ac.in>
2025-06-24 15:35:54 +0530

    Word Capitalization solution

---
+++ Implementation-Constructive/Word Capitalization.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin>>s;
+ 
+    if(s[0]>='a'&& s[0]<='z'){
+        int i = s[0]-'a';
+        s[0]='A'+i; 
+    }
+    cout<<s<<"\n";
+    return 0;
+}

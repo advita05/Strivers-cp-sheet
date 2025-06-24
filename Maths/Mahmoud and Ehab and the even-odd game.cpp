4eb9f9af1034263409c3716281b41b34af4a59d2
Advita Talekar <advita.18060@sakec.ac.in>
2025-06-24 19:32:03 +0530

    Mahmoud and Ehab and the even-odd game solution

---
+++ Maths/Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n;
+    cin >> n;
+    
+    if(n%2!=0) cout<<"Ehab";
+    else cout<<"Mahmoud";
+    return 0;
+}

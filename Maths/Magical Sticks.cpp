109a64dfc3dd5154f2a23dd9d50276ccec210e2c
Advita Talekar <advita.18060@sakec.ac.in>
2025-06-29 20:40:24 +0530

    Magical Sticks solution

---
+++ Maths/Magical Sticks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t, n;
+    cin>>t;
+    while (t--) {
+        cin>>n;
+        int res = (n+1)/2;
+        cout<<res<<endl;
+    }
+    return 0;
+}

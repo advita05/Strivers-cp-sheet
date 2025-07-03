80061c2dee1e08810257e0584b8eedce55ec6e0a
Advita Talekar <advita.18060@sakec.ac.in>
2025-07-03 17:37:30 +0530

    Domino Piling solution

---
+++ Maths/Domino Piling.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    int res = abs(x * y) / 2;
+    cout << res;
+
+    return 0;
+}

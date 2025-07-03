060c743dfce4e5eaafb17fd240f8940499e5ccde
Advita Talekar <advita.18060@sakec.ac.in>
2025-07-03 13:13:58 +0530

    Candies and Two Sisters solution

---
+++ Maths/Candies and Two Sisters.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ans = 0;
+        ans = ceil(n-1) / 2;
+        cout << ans << endl;
+    }
+    return 0;
+}
